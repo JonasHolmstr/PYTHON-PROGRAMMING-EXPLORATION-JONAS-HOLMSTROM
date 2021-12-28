aa76fffd0ae86b4e681e9c142cef8addc61c93a4
JonasHolmstr <joholmstr@gmail.com>
2021-12-28 21:23:16 +0100

    adding rectangle class

--- APP 1/main.py
+++ APP 1/main.py
@@ -1,1 +1,30 @@
-print("test")
+from random import randint
+
+rectangle = Rectangle(
+    Point(randint(0, 9), randint(0, 9)),
+    Point(randint(10, 19), randint(10, 19)))
+
+
+class Point:
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+    def falls_in_rectangle(self, rectangle):
+        if rectangle.lowleft.x < self.x < \
+        rectangle.upright.x \
+        and rectangle.lowleft.y < self.y < rectangle.upright.y:
+             return True
+        else:
+             return False
+
+
+class Rectangle:
+
+    def __init__(self, lowleft, upright):
+        self.lowleft = lowleft
+        self.upright = upright
+
+
